89ef6f80af1202f37eeb123c2e83caf34b953b14
Julie K <you@example.com>
2016-11-22 16:37:30 +0100

    up

--- exercises/004/solution.py
+++ exercises/004/solution.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python
 import string
-chaine = string.ascii_lowercase[::-1]
-n = chaine.replace('a','A')
-j = n.replace('e','E')
-p = j.replace('i','I')
-m = p.replace('o','O')
-k = m.replace('u','U')
-print(k)
+n = string.ascii_lowercase[::-1]
+j = n.replace('a','A')
+m = j.replace('e','E')
+l = m.replace('i','I')
+k = l.replace('o','O')
+p = k.replace('u','U')
+print(p)
+
