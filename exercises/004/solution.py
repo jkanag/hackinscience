2a46c9a151a3945a9e83fa271b8882ce2b3fee62
Julie K <you@example.com>
2016-11-22 16:02:12 +0100

    up

--- exercises/004/solution.py
+++ exercises/004/solution.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python
 import string
-from string import maketrans
-table = maketrans('aeiou','AEIOU')
-chaine =string.ascii_lowercase[::-1]
-print(chaine.translate(table,' '))
+chaine = string.ascii_lowercase[::-1]
+n = chaine.replace('a','A')
+j = n.replace('e','E')
+p = j.replace('i','I')
+m = p.replace('o','O')
+k = m.replace('u','U')
+print(k)
