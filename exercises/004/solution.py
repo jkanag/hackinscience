5b7b250c21484edd666d2470befb929ef14692aa
Julie K <you@example.com>
2016-11-22 15:37:05 +0100

    up

--- exercises/004/solution.py
+++ exercises/004/solution.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python
 import string
-print(string.ascii_lowercase[::-1])
+from string import maketrans
+table = maketrans('aeiou','AEIOU')
+chaine =string.ascii_lowercase[::-1]
+print chaine.translate(table,' ')
