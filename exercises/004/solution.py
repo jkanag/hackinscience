9d4ab93eeef27abbd654fd569ca7e269c63f4987
Julie K <you@example.com>
2016-12-02 16:08:16 +0100

    up

--- exercises/004/solution.py
+++ exercises/004/solution.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
 import string
 n = string.ascii_lowercase[::-1]
-j = n.replace('a','A')
-m = j.replace('e','E')
-l = m.replace('i','I')
-k = l.replace('o','O')
-p = k.replace('u','U')
+j = n.replace('a', 'A')
+m = j.replace('e', 'E')
+l = m.replace('i', 'I')
+k = l.replace('o', 'O')
+p = k.replace('u', 'U')
 print(p)
