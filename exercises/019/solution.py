04c51415de016149471866cefbc72d170a5b0040
Julie K <you@example.com>
2016-12-16 14:31:37 +0100

    up

--- exercises/019/solution.py
+++ exercises/019/solution.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python
 import sys
-x = sys.argv[1]
-y = sys.argv[2]
 i = (len(sys.argv)-1)
 if (i == 0):
     print("usage: python3 solution.py OP1 OP2")
 if (i == 1):
     print("usage: python3 solution.py OP1 OP2")
 else:
-    print(int(x) + int(y))
+    print(int(sys.argv[1]) + int(sys.argv[2]))
